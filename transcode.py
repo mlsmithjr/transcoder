da8876780077a52cdcfb82c3e861630b065c0039
Mark Smith <marshallsmithjr@gmail.com>
2018-07-20 20:33:08 -0400

    bug fixes, added -v flag

--- transcode.py
+++ transcode.py
@@ -8,10 +8,12 @@ import subprocess
 from queue import Queue
 from threading import Thread
 
+
 DEFAULT_CONFIG = os.path.expanduser('~/.transcode.yml')
 
-valid_predicates = ['vcodec', 'res_height', 'res_width', 'runtime', 'source_size', 'fps']
-video_re = re.compile('^.*Duration: (\d+):(\d+):.* Stream #0:0.*: Video: (\w+).*, (\d+)x(\d+).* (\d+) fps,.*$', re.DOTALL)
+valid_predicates = ['vcodec', 'res_height', 'res_width', 'runtime', 'filesize_mb', 'fps']
+video_re = re.compile('^.*Duration: (\d+):(\d+):.* Stream #0:0.*: Video: (\w+).*, (\d+)x(\d+).* (\d+)(\.\d.)? fps,.*$',
+                      re.DOTALL)
 thread_queue = Queue(10)
 complete = set()
 queue_path = None
@@ -21,6 +23,7 @@ config = dict()
 concurrent_jobs = 2
 keep_source = False
 dry_run = False
+verbose = False
 
 
 class MediaInfo:
@@ -63,10 +66,14 @@ class MediaInfo:
         return self._fps
 
 
-def match_profile(path, mediainfo) -> (str, str):
+def match_profile(mediainfo: MediaInfo) -> (str, str):
+    global verbose
+
     for description, body in matching_rules.items():
+        if verbose: print(f' > evaluating "{description}"')
         if 'rules' not in body:
             # no rules section, match by default
+            if verbose: print(f'  >> rule {description} selected by default (no criteria)')
             return body['profile'], description
         for pred, value in body['rules'].items():
             inverted = False
@@ -74,14 +81,18 @@ def match_profile(path, mediainfo) -> (str, str):
                 print(f'Invalid predicate {pred} in rule {description}')
                 exit(1)
             if isinstance(value, str) and len(value) > 1 and value[0] == '!':
-              inverted = True
-              value = value[1:]
+                inverted = True
+                value = value[1:]
             if pred == 'vcodec' and mediainfo.vcodec != value and not inverted:
+                if verbose:
+                    print(f'  >> predicate vcodec ("{value}") did not match {mediainfo.vcodec}')
                 break
             if pred == 'path':
                 try:
                     m = re.match(mediainfo.path, value)
                     if m is None:
+                        if verbose:
+                            print(f'  >> predicate path ("{value}") did not match {mediainfo.path}')
                         break
                 except Exception as ex:
                     print(f'invalid regex {mediainfo.path} in rule {description}')
@@ -90,26 +101,36 @@ def match_profile(path, mediainfo) -> (str, str):
                 if value.isnumeric():
                     value = '==' + value  # make python-friendly
                 if not eval(f'{mediainfo.res_height}{value}'):
+                    if verbose:
+                        print(f'  >> predicate res_height ("{value}") did not match {mediainfo.res_height}')
                     break
             if pred == 'res_width' and len(value) > 1:
                 if value.isnumeric():
                     value = '==' + value  # make python-friendly
                 if not eval(f'{mediainfo.res_width}{value}'):
+                    if verbose:
+                        print(f'  >> predicate res_width ("{value}") did not match {mediainfo.res_width}')
                     break
             if pred == 'runtime' and len(value) > 1:
                 if value.isnumeric():
                     value = '==' + value  # make python-friendly
                 if not eval(f'{mediainfo.runtime}{value}'):
+                    if verbose:
+                        print(f'  >> predicate runtime ("{value}") did not match {mediainfo.runtime}')
                     break
             if pred == 'fps' and len(value) > 1:
                 if value.isnumeric():
                     value = '==' + value  # make python-friendly
                 if not eval(f'{mediainfo.fps}{value}'):
+                    if verbose:
+                        print(f'  >> predicate fps ("{value}") did not match {mediainfo.fps}')
                     break
-            if pred == 'source_size' and len(value) > 1:
+            if pred == 'filesize_mb' and len(value) > 1:
                 if value.isnumeric():
                     value = '==' + value  # make python-friendly
                 if not eval(f'{mediainfo.filesize_mb}{value}'):
+                    if verbose:
+                        print(f'  >> predicate filesize_mb ("{value}") did not match {mediainfo.filesize_mb}')
                     break
         else:
             # didn't bail out on any predicates, have a match
@@ -126,17 +147,18 @@ def loadq(queuepath) -> list:
         return _files
 
 
-def fetch_details(_path) -> MediaInfo:
-    with subprocess.Popen(['ffmpeg', '-i', path], stderr=subprocess.PIPE) as proc:
+def fetch_details(_path: str) -> MediaInfo:
+    with subprocess.Popen(['ffmpeg', '-i', _path], stderr=subprocess.PIPE) as proc:
         output = proc.stderr.read().decode(encoding='utf8')
         match = video_re.match(output)
-        if match is None or len(match.groups()) != 6:
+        if match is None or len(match.groups()) < 6:
             print(f'>>>> regex match on video stream data failed: ffmpeg -i {_path}')
-            return MediaInfo(path, None, 0, 0, 0, 0, 0)
+            return MediaInfo(_path, None, 0, 0, 0, 0, 0)
         else:
             _dur_hrs, _dur_mins, _codec, _res_width, _res_height, fps = match.group(1, 2, 3, 4, 5, 6)
-            filesize = os.path.getsize(path) / (1024 * 1024)
-            return MediaInfo(_path, _codec, int(_res_width), int(_res_height), (int(_dur_hrs) * 60) + int(_dur_mins), filesize, int(fps))
+            filesize = os.path.getsize(_path) / (1024 * 1024)
+            return MediaInfo(_path, _codec, int(_res_width), int(_res_height), (int(_dur_hrs) * 60) + int(_dur_mins),
+                             filesize, int(fps))
 
 
 def perform_transcodes():
@@ -184,6 +206,77 @@ def load_config(_path):
         concurrent_jobs = config['concurrent_jobs']
 
 
+def notify_plex():
+    global config
+
+    if 'plex_server' in config and config['plex_server'] is not None and not dry_run:
+        plex_server = config['plex_server']
+        try:
+            from plexapi.server import PlexServer
+
+            plex = PlexServer('http://{}'.format(plex_server))
+            plex.library.update()
+            # plex.library.section(PLEX_DEFAULT_REFRESH_LIBRARY).update()
+        except ModuleNotFoundError as ex:
+            print(
+                'Library not installed. To use Plex notifications please install the Python 3 Plex API ' +
+                '("pip3 install plexapi")')
+        except Exception as ex2:
+            print(f'Unable to connect to Plex server at {plex_server}')
+
+
+def enqueue_files(files: list):
+
+    for path, forced_profile in files:
+        #
+        # do some prechecks...
+        #
+        if forced_profile is not None and forced_profile not in profiles:
+            print(f'profile "{forced_profile}" referenced from command line not found')
+            exit(1)
+
+        if len(path) == 0:
+            continue
+        if not os.path.isfile(path):
+            print('path not found, skipping: ' + path)
+            continue
+
+        path = os.path.abspath(path)  # convert to full path so that rule filtering can work
+        print('processing ' + path)
+        minfo = fetch_details(path)
+        if minfo.vcodec is not None:
+            if forced_profile is None:
+                matched_profile, rule = match_profile(minfo)
+                if matched_profile is None:
+                    print(f'No matching profile found - skipped')
+                    continue
+                if matched_profile.upper() == 'SKIP':
+                    print(f'Skipping due to profile rule: {rule}')
+                    complete.add(path)
+                    continue
+                if matched_profile not in profiles:
+                    print(f'profile "{matched_profile}" referenced from rule "{rule}" not found')
+                    exit(1)
+                the_profile = profiles[matched_profile]
+                outpath = path[0:path.rfind('.')] + the_profile['extension'] + '.tmp'
+                thread_queue.put((path, outpath, matched_profile))
+            else:
+                #
+                # looks good, add this file to the thread queue
+                #
+                the_profile = profiles[forced_profile]
+                outpath = path[0:path.rfind('.')] + the_profile['extension'] + '.tmp'
+                thread_queue.put((path, outpath, forced_profile))
+
+            # if vcodec in ('hevc', 'x265', 'h265'):
+            #     print('found h265, skipping: ' + path)
+            #     complete.add(path)
+            #     continue
+            # if int(res_height) < 720:
+            #     print('low resolution video will not be transcoded')
+            #     complete.add(path)
+            #     continue
+
 if __name__ == '__main__':
 
     if len(sys.argv) == 2 and sys.argv[1] == '-h':
@@ -197,6 +290,7 @@ if __name__ == '__main__':
         print('OPTIONS:')
         print('  -s         Process files sequentially even if configured for multiple concurrent jobs')
         print('  --dry-run  Run without actually transcoding or modifying anything, useful to test rules and profiles')
+        print('  -v         Verbose output, helpful in debugging profiles and rules')
         print(
             '  -k         Keep source files after transcoding. If used, the transcoded file will have the same name and .tmp extension')
         print('  -y <file>  Full path to configuration file.  Default is ~/.transcode.yml')
@@ -238,6 +332,8 @@ if __name__ == '__main__':
                 keep_source = True
             elif sys.argv[arg] == '--dry-run':
                 dry_run = True
+            elif sys.argv[arg] == '-v':
+                verbose = True
             else:
                 files.append((sys.argv[arg], profile))
             arg += 1
@@ -253,57 +349,9 @@ if __name__ == '__main__':
     if files is None:
         exit(0)
 
-    for path, forced_profile in files:
-        #
-        # do some prechecks...
-        #
-        if forced_profile is not None and forced_profile not in profiles:
-            print(f'profile "{forced_profile}" referenced from command line not found')
-            exit(1)
-
-        if len(path) == 0:
-            continue
-        if not os.path.isfile(path):
-            print('path not found, skipping: ' + path)
-            continue
-
-        path = os.path.abspath(path)	# convert to full path so that rule filtering can work
-        print('processing ' + path)
-        minfo = fetch_details(path)
-        if minfo.vcodec is not None:
-            if forced_profile is None:
-                matched_profile, rule = match_profile(path, minfo)
-                if matched_profile is None:
-                    print(f'No matching profile found - skipped')
-                    continue
-                if matched_profile.upper() == 'SKIP':
-                    print(f'Skipping due to profile rule: {rule}')
-                    complete.add(path)
-                    continue
-                if matched_profile not in profiles:
-                    print(f'profile "{matched_profile}" referenced from rule "{rule}" not found')
-                    exit(1)
-                the_profile = profiles[matched_profile]
-                outpath = path[0:path.rfind('.')] + the_profile['extension'] + '.tmp'
-                thread_queue.put((path, outpath, matched_profile))
-            else:
-                #
-                # looks good, add this file to the thread queue
-                #
-                the_profile = profiles[forced_profile]
-                outpath = path[0:path.rfind('.')] + the_profile['extension'] + '.tmp'
-                thread_queue.put((path, outpath, forced_profile))
-
-            # if vcodec in ('hevc', 'x265', 'h265'):
-            #     print('found h265, skipping: ' + path)
-            #     complete.add(path)
-            #     continue
-            # if int(res_height) < 720:
-            #     print('low resolution video will not be transcoded')
-            #     complete.add(path)
-            #     continue
-
+    enqueue_files(files)
     print()
+
     #
     # all files are listed in the queue so start the threads
     #
@@ -328,17 +376,9 @@ if __name__ == '__main__':
         else:
             os.remove(queue_path)
 
-    if 'plex_server' in config and config['plex_server'] is not None and not dry_run:
-        try:
-            from plexapi.server import PlexServer
+    notify_plex()
 
-            plex = PlexServer('http://{}'.format(config['plex_server']))
-            plex.library.update()
-            # plex.library.section(PLEX_DEFAULT_REFRESH_LIBRARY).update()
-        except Exception as ex:
-            print(
-                'Library not installed. To use Plex notifications please install the Python 3 Plex API ' +
-                '("pip3 install plexapi")')
+
 
 SAMPLE_YAML = """
 ##
@@ -348,17 +388,17 @@ config:
   default_queue_file: '/volume1/config/sonarr/transcode_queue.txt'
   ffmpeg: '/usr/bin/ffmpeg'
   concurrent_jobs: 2
-  plex_server: null 
+  plex_server: null           # can be 'server:port'
 
 ##
-# profile definitions
+# profile definitions.  You can model all your transcoding combinations here.
 ##
-
 profiles:
-  hevc_hd_preserved:          # default for almost everything
+  hevc_hd_preserved:          # what I use for almost everything
       input_options: |
         -hide_banner
         -nostats
+        -loglevel quiet
         -hwaccel cuvid
       output_options: |
         -c:v hevc_nvenc
@@ -368,11 +408,13 @@ profiles:
         -c:a copy
         -c:s copy
         -f matroska
+      extension: '.mkv'
 
-  hevc_hd_25fps:               # when movie source is just too big, cut down fps
+  hevc_25fps:               # when movie source is just too big, cut down fps
       input_options: |
          -hide_banner
          -nostats
+         -loglevel quiet
          -hwaccel cuvid
       output_options: |
         -c:v hevc_nvenc
@@ -383,11 +425,30 @@ profiles:
         -c:s copy
         -f matroska
         -r 25
+      extension: '.mkv'
+
+  hevc_30fps:               # when movie source is just too big, cut down fps
+      input_options: |
+         -hide_banner
+         -nostats
+         -loglevel quiet
+         -hwaccel cuvid
+      output_options: |
+        -c:v hevc_nvenc
+        -profile:v main
+        -preset medium
+        -crf 20
+        -c:a copy
+        -c:s copy
+        -f matroska
+        -r 30
+      extension: '.mkv'
 
   hevc_hd_lq:                 # lower quality, for when source material isn't that good anyhow
       input_options: |
          -hide_banner
          -nostats
+         -loglevel quiet
          -hwaccel cuvid
       output_options: |
         -c:v hevc_nvenc
@@ -397,7 +458,19 @@ profiles:
         -c:a copy
         -c:s copy
         -f matroska
-
+      extension: '.mkv'
+  x264:                    # basic x264 transcode using CPU (no CUDA support)
+      input_options: |
+         -hide_banner
+         -nostats
+         -loglevel quiet
+      output_options: |
+        -c:v x264
+        -crf 22
+        -c:a copy
+        -c:s copy
+        -f mp4
+      extension: '.mp4'
 
 #
 # Automatching happens when a profile isn't provided on the command line.  These rules are evalulated to find the
@@ -405,19 +478,13 @@ profiles:
 #
 # rule predicates:
 #
-#  Use:
-#      predicate: value
-#
-#  Where:
-#
-#      predicate        one of the supported values:
-#                           vcodec         Video codec of the source ('ffmpeg -codecs' to see full list)
-#                           res_height     Source video resolution height, operators < and > allowed
-#                           res_width      Source video resolution width, operators < and > allowed
-#                           source_size    Size of the source file (in megabytes), operators allowed
-#                           runtime        Source runtime in minutes, operators allowed
-#                           path           Full path of the source file. Value can be a regular expression.
-#      value            what to match the predicate against. Simple values are equality tests but numerics can also have '<' or '>'
+#            vcodec         Video codec of the source ('ffmpeg -codecs' to see full list), may preceed with ! for not-equal test
+#            res_height     Source video resolution height, operators < and > allowed
+#            res_width      Source video resolution width, operators < and > allowed
+#            filesize_mb    Size of the source file (in megabytes), operators allowed
+#            runtime        Source runtime in minutes, operators allowed
+#            fps            Framerate of the source
+#            path           Full path of the source file. Value can be a regular expression (ie. '.*/Television/.*').
 #
 # Rules are evaluated in order.  First matching rule wins so order wisely.
 # Rules with a profile of "SKIP" mean to skip processing of the matched video
@@ -426,20 +493,31 @@ rules:
   'skip video if already encoded in hevc/h265':
       profile: SKIP
       rules:
-        codec: 'hevc'
+        vcodec: 'hevc'
 
-  'skip video if resolution < 720 (do not bother transcoding)':
+  'high frame rate':
+      profile: hevc_30fps
+      rules:
+        fps: '>30'
+        filesize_mb: '>500'
+
+  'skip video if resolution < 700':
       profile: SKIP
       rules:
-        res_height: '<720'
+        res_height: '<700'
 
-  'for content I consider too big for their runtime':
+  'content just too big and framey':
       profile: hevc_hd_25fps
       rules:
         runtime:      '<180'      # less than 3 hours
-        source_size:  '>5000'  # ..and larger than 5 gigabytes
+        filesize_mb:  '>6000'  # ..and larger than 6 gigabytes
+        fps: '>25'
 
   'default':    # this will be the DEFAULT (no rules implies a match)
       profile: hevc_hd_preserved
+      rules:
+        vcodec: '!hevc'
+
+
 
 """
